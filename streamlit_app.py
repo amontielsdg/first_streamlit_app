baad3451b1a10349fdb880f06c1fe650f7fe1eb9
amontielsdg <117348112+amontielsdg@users.noreply.github.com>
2022-11-11 10:40:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.title("My parents new healthy diner")
 
 streamlit.header("Breakfast")
 streamlit.text("Churros con chocolate saludable")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
